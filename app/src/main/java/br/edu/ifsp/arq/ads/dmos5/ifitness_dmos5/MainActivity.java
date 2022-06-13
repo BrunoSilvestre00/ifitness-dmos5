9673d1fd0c89f45e6cccba8bc91f72339e1f8f6c
Brunosilvestre00 <bruno.ap.silvestre@gmail.com>
2022-06-13 09:58:53 -0300

    commit inicial

---
+++ app/src/main/java/br/edu/ifsp/arq/ads/dmos5/ifitness_dmos5/MainActivity.java
@@ -0,0 +1,14 @@
+package br.edu.ifsp.arq.ads.dmos5.ifitness_dmos5;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
