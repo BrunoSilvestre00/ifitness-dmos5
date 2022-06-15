b158cac46c5f8f9cfe7ec062f06a3c9dafd7ffa5
Bianca Seolin <bianmca.seolin@aluni.ifsp.edu.br>
2022-06-15 20:06:47 -0300

    Tela login feature-10 (NÃO COLOCAR NA BRANCHE PRINCIPAL)

---
+++ app/src/main/java/br/edu/ifsp/arq/ads/dmos5/ifitness_dmos5/activitys/LoginActivity.java
@@ -0,0 +1,16 @@
+package br.edu.ifsp.arq.ads.dmos5.ifitness_dmos5.activitys;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+import br.edu.ifsp.arq.ads.dmos5.ifitness_dmos5.R;
+
+public class LoginActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_login);
+    }
+}
